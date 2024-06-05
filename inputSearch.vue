cebc925be4e4689da9b757d626b8c211953155cb
Martin Elias <82126761+Martin10123@users.noreply.github.com>
2024-06-05 16:23:29 -0500

    Update inputSearch.vue
    a

--- inputSearch.vue
+++ inputSearch.vue
@@ -1,128 +1,73 @@
-<template>
-  <div class="relative">
-    <div class="flex items-center">
-      <input
-        type="text"
-        class="border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        placeholder="Buscar..."
-        v-model="search"
-        @input="filterOptions"
-      />
-    </div>
+APP_NAME=Laravel
+APP_ENV=local
+APP_KEY=base64:TYcCA+kH0PWDNQQngvoCv6qFwDwzIG2tQ045i8CDoFc=
+APP_DEBUG=true
+APP_TIMEZONE=UTC
+APP_URL=http://localhost
 
-    <!-- Contenedor para mostrar las opciones seleccionadas -->
-    <div
-      class="flex gap-1 snap-x snap-mandatory m-2 overflow-x-auto p-2 shadow-md bg-white rounded-md"
-      v-if="optionsSelected.length > 0"
-    >
-      <span
-        v-for="option in optionsSelected"
-        :key="option.toString()"
-        @click="() => onCopyOption(option.toString())"
-        class="flex items-center justify-center inline-block cursor-pointer bg-slate-100 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 snap-always shrink-0 snap-center"
-      >
-        {{ option }}
-        <button
-          class="flex items-center justify-center ml-2 bg-transparent text-red-500 bg-slate-200 text-gray-700 rounded-full transition-colors duration-300"
-          @click="() => removeSelectedOption(option.toString())"
-        >
-          X
-        </button>
-      </span>
-    </div>
+APP_LOCALE=en
+APP_FALLBACK_LOCALE=en
+APP_FAKER_LOCALE=en_US
 
-    <ul
-      class="absolute w-full bg-white border border-gray-300 rounded-md mt-1 shadow-md max-h-80 overlay-y-scroll overflow-y-auto z-10"
-      v-if="search.length > 0"
-    >
-      <li
-        class="py-2 px-3 hover:bg-gray-100 cursor-pointer"
-        v-for="option in filteredOptions"
-        :key="option"
-        :class="{ 'bg-blue-300 hover:bg-blue-400': optionsSelected.includes(option) }"
-        @click="() => onSelectOption(option)"
-      >
-        {{ option }}
+APP_MAINTENANCE_DRIVER=file
+APP_MAINTENANCE_STORE=database
 
-        <span v-if="optionsSelected.includes(option)" class="float-right text-white"> ✓ </span>
-      </li>
-    </ul>
-  </div>
-</template>
+BCRYPT_ROUNDS=12
 
-<script setup lang="ts">
-import { useInputSearch } from '@/composables/useInputSearch';
-import { ref } from 'vue';
+LOG_CHANNEL=stack
+LOG_STACK=single
+LOG_DEPRECATIONS_CHANNEL=null
+LOG_LEVEL=debug
 
-const { options, searchP } = defineProps<{
-  options: string[];
-  searchP: string;
-}>();
+DB_CONNECTION=sqlsrv
+DB_HOST=HIPNOS\MSSQLGEDIN
+DB_PORT=54338
+DB_DATABASE=SOLSERV
+DB_USERNAME=gedinconn
+DB_PASSWORD=Cotecmar2018
 
-const search = ref(searchP);
+SESSION_DRIVER=database
+SESSION_LIFETIME=120
+SESSION_ENCRYPT=false
+SESSION_PATH=/
+SESSION_DOMAIN=null
 
-const {
-  filterOptions,
-  filteredOptions,
-  onCopyOption,
-  onSelectOption,
-  optionsSelected,
-  removeSelectedOption,
-} = useInputSearch({ search: search.value, options });
-</script>
+BROADCAST_CONNECTION=log
+FILESYSTEM_DISK=local
+QUEUE_CONNECTION=database
 
-// useInputSearch
+CACHE_STORE=database
+CACHE_PREFIX=
 
-import { ref } from 'vue';
+MEMCACHED_HOST=127.0.0.1
 
-export const useInputSearch = ({ search, options }: {
-    search: string,
-    options: string[]
-}) => {
-    const optionsSelected = ref<String[]>([]);
+REDIS_CLIENT=phpredis
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
 
-    const filteredOptions = ref(options);
+MAIL_MAILER=log
+MAIL_HOST=127.0.0.1
+MAIL_PORT=2525
+MAIL_USERNAME=null
+MAIL_PASSWORD=null
+MAIL_ENCRYPTION=null
+MAIL_FROM_ADDRESS="hello@example.com"
+MAIL_FROM_NAME="${APP_NAME}"
 
-    const removeAccents = (str: string) => {
-        return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    };
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_DEFAULT_REGION=us-east-1
+AWS_BUCKET=
+AWS_USE_PATH_STYLE_ENDPOINT=false
 
-    const filterOptions = () => {
-        const normalizedSearch = removeAccents(search.toLowerCase());
+VITE_APP_NAME="${APP_NAME}"
 
-        filteredOptions.value = options.filter((option) => {
-            const normalizedOption = removeAccents(option.toLowerCase());
-            return normalizedOption.includes(normalizedSearch);
-        });
-
-        if (filteredOptions.value.length === 0) {
-            filteredOptions.value = ['No se encontraron resultados'];
-        }
-    };
-
-    const onSelectOption = (option: string) => {
-        if (optionsSelected.value.includes(option)) {
-            optionsSelected.value = optionsSelected.value.filter((item) => item !== option);
-        } else {
-            optionsSelected.value = [...optionsSelected.value, option];
-        }
-    };
-
-    const removeSelectedOption = (option: string) => {
-        optionsSelected.value = optionsSelected.value.filter((item) => item !== option);
-    };
-
-    const onCopyOption = (option: string) => {
-        navigator.clipboard.writeText(`Cotecmar/${option}`);
-    };
-
-    return {
-        optionsSelected,
-        filteredOptions,
-        filterOptions,
-        onSelectOption,
-        removeSelectedOption,
-        onCopyOption,
-    };
-
-}
+LDAP_HOST=artemisa.cotecmar.com 
+LDAP_USERNAME=appda@cotecmar.com 
+LDAP_PASSWORD=C0t3cm4r2019* 
+LDAP_PORT=389 
+LDAP_BASE_DN=dc=cotecmar,dc=com 
+LDAP_TIMEOUT=5 
+LDAP_SSL=false 
+LDAP_TLS=false 
